fdeaabcd87a872033532a5938e29455337d847e7
louysa <doganlouysa@outlook.com>
2025-03-18 17:20:03 +0300

    redirect tested

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -4,13 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-
-
-
-
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class DemoController {
@@ -48,4 +45,32 @@ public class DemoController {
         return mv;
     }
     
+    
+    @GetMapping({"/display-form","/form.html" })
+    public String displayForm(Model model){
+        model.addAttribute("person", new Person());
+        return "form";
+
+    }
+    
+    @PostMapping("/send-values")
+    public String sendForm(@ModelAttribute Person person, Model model) {
+        model.addAttribute("person", person);
+        return "personresult";
+    }
+    
+    @PostMapping("/send-values-redirect")
+    public String sendFormRedirect(@ModelAttribute Person person, RedirectAttributes attributes){
+        attributes.addFlashAttribute("person",person);
+        return "redirect:/result";
+    }
+
+    @GetMapping("/result")
+    public String showResult(Model model){
+
+        if(!model.containsAttribute("person")){
+            return "redirect:/form.html";
+        }
+        return "personresult";
+    }
 }

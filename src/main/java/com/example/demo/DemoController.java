ed6a1f14d8867cd7c7fe117b6d7d5369b0260c7d
louysa <doganlouysa@outlook.com>
2025-03-18 15:28:45 +0300

    Model, ModelMap and ModelAndView tested

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,16 +1,51 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+
 
 @Controller
 public class DemoController {
-    @GetMapping("")
-    @ResponseBody
-    public String requestHandler(@RequestParam(value = "isim",required=false) String val) {
-        return val;
+
+    private final DemoApplication demoApplication;
+
+    DemoController(DemoApplication demoApplication) {
+        this.demoApplication = demoApplication;
     }
+    @GetMapping("/show")
+    public String showView() {
+        return "view-1" ;
+    }
+
+    @GetMapping("/modeltest")
+    public String modeltest(Model model) {
+        model.addAttribute("attrib1","Value 1 from ModelTest");
+        model.addAttribute("attrib2","Value 2 from ModelTest");
+        return "result" ;
+    }
+
+    @GetMapping("/modelmaptest")
+    public String modelmaptest(ModelMap modelMap){
+        modelMap.addAttribute("attrib1", "Value 1 From ModelMapTest");
+        modelMap.addAttribute("attrib2", "Value 2 From ModelMapTest");
+        return "result";
+    }
+
+    @GetMapping("/modelandviewtest")
+    public ModelAndView modelandviewtest(){
+        ModelAndView mv = new ModelAndView("result");
+
+        mv.addObject("attrib1", "Value 1 From ModelAndViewTest");
+        mv.addObject("attrib2", "Value 2 From ModelAndViewTest");
+        return mv;
+    }
+    
 }
